5cbb58d4f5680a50d150a00a91d6b64111816779
loay fayez <84818419+leon019i@users.noreply.github.com>
2021-12-06 10:15:11 +0200

    loay
    commit

--- src/MyThread.java
+++
@@ -1,8 +0,0 @@
-public class MyThread extends Thread{
-    String name;
-    MyThread(String name) { this.name = name;}
-    @Override
-    public void run() { for (int i = 0; i < 1000; i++)
-        System.out.println(name + " is running!");
-    }
-}
