5b2806cf4fcd010d1b894eeb6c6a4c9da82c1020
loay fayez <84818419+leon019i@users.noreply.github.com>
2021-12-06 10:49:16 +0200

    Main editing
    done some main works remain the loop of the user experience.

--- src/Network.java
+++ src/Network.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Network {
     public static void main(String[] args) {
         boolean flag =true;
+        int N,TC;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        String TCline;
+        System.out.println("What is the number of WI-FI Connections?");
+        N = scanner.nextInt();
+        System.out.println("What is the number of devices Clients want to connect?");
+        TC = scanner.nextInt();
+        Semaphore semaphore_live = new Semaphore(N);
+        Router router_live;
+        int devices=0;
         while (flag){
+            while (devices>=TC)
+            String line = scanner.nextLine();
+            
+
 
 
         }
--- src/Router.java
+++ src/Router.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Router{
-    int capacity = 0;
     List<Device> list=new ArrayList<>();
 
 }
